996dda4d9f7317b2a727f1fa9627a52191189b9b
LuccaRh <luccah57@gmail.com>
2023-06-27 08:41:19 -0300

    id,add, table

--- Projeto/src/Projeto/GuiaComAbas.java
+++ Projeto/src/Projeto/GuiaComAbas.java
@@ -9,11 +9,9 @@ import java.util.Objects;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import javax.swing.table.DefaultTableModel;
-import java.util.Random;
 
 public class GuiaComAbas extends JFrame{
-	Random random = new Random();
-    private int id;
+    private static int id = 0;
 	public String path;
     public GuiaComAbas(String path){
         super("Exemplo de Abas");
@@ -67,10 +65,6 @@ public class GuiaComAbas extends JFrame{
         inputPanel.add(bananaTextField);
         inputPanel.add(new JLabel("Uva(kg):"));
         inputPanel.add(uvaTextField);
-        nomeTextField.setText("");
-        macaTextField.setText("");
-        bananaTextField.setText("");
-        uvaTextField.setText("");
 
         attPanel.add(new JLabel("ID:"));
         attPanel.add(attIdTextField);
@@ -82,11 +76,6 @@ public class GuiaComAbas extends JFrame{
         attPanel.add(attBananaTextField);
         attPanel.add(new JLabel("Uva(kg):"));
         attPanel.add(attUvaTextField);
-        attIdTextField.setText("");
-        attNomeTextField.setText("");
-        attMacaTextField.setText("");
-        attBananaTextField.setText("");
-        attUvaTextField.setText("");
         
         adicionarPanel.setLayout(new BoxLayout(adicionarPanel, BoxLayout.Y_AXIS));
         adicionarPanel.add(inputPanel);
@@ -110,11 +99,7 @@ public class GuiaComAbas extends JFrame{
                 String uva = uvaTextField.getText();
 
                 adicionarRegistroCSV(nome, maca, banana, uva);
-                
-                double macad = Double.parseDouble(maca)*TabelaValores.MACA.getValor();
-                double bananad = Double.parseDouble(banana)*TabelaValores.BANANA.getValor(); 
-                double uvad = Double.parseDouble(uva)*TabelaValores.UVA.getValor();
-                
+
                 // Limpar os campos de texto após adicionar o registro
                 nomeTextField.setText("");
                 macaTextField.setText("");
@@ -123,7 +108,7 @@ public class GuiaComAbas extends JFrame{
 
                 // Atualizar a tabela após adicionar o registro
                 DefaultTableModel tableModel = (DefaultTableModel) tabela.getModel();
-                tableModel.addRow(new Object[]{id, nome, macad, bananad, uvad}); 
+                tableModel.addRow(new Object[]{nome, maca, banana, uva});
             }
         });
         atualizarButton.addActionListener(new ActionListener(){
@@ -169,9 +154,7 @@ public class GuiaComAbas extends JFrame{
         String caminhoArquivo = this.path + "projetoMC322/Projeto/src/Projeto/Nome.csv";
 
         try(BufferedWriter writer = new BufferedWriter(new FileWriter(caminhoArquivo, true))){
-        	int numeroInteiro = random.nextInt(100000000);
-        	id = numeroInteiro;
-            String novoRegistro = id + "," 
+            String novoRegistro = id++ + "," 
                                 + nome + "," 
                                 + Double.parseDouble(maca)*TabelaValores.MACA.getValor() + "," 
                                 + Double.parseDouble(banana)*TabelaValores.BANANA.getValor() + "," 
